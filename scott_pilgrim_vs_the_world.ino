719ba0784c7aac473d03efd1b33eaa61258545a9
MSee-Mikey <michaelacauthen@icloud.com>
2026-04-01 16:47:17 -0500

    Add files via upload
    first file

---
+++ scott_pilgrim_vs_the_world.ino
@@ -0,0 +1,12 @@
+// BASE FILE FOR SCOTT PILGRIM VS THE WORLD ARDUINO PROGJECT
+
+void setup() {
+  // put your setup code here, to run once:
+  // imagine code here, amazing code
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // imagine code here
+}
